ddbbf676fba094eed90b27db4b769348f07c89f6
SebastianCaceres <sebastiancaceres071@gmail.com>
2024-12-14 09:07:37 -0500

    Revert "Merge branch 'restor_changes' into configForCNC"
    This reverts commit e2e76de8541371d6b7f9934a1e8d987111c65568, reversing
    changes made to 7b6eac2c8170557717a2758fbddb32cb361385b5.
    
     Changes to be committed:
    	modified:   Marlin/Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -66,7 +66,7 @@
 //
 //===========================================================================
 
-// @section info test
+// @section info
 
 // Author info of this build printed to the host during boot and M115
 #define STRING_CONFIG_H_AUTHOR "(BigTreeTech, SKR-mini-E3-V3.0)" // Who made the changes.
